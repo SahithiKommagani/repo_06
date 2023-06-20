7f9d131abaf4c60ed28a62bb893d4d468ddeec13
Sahithi Kommagani <sahithi.kommagani@hitachivantara.com>
2023-06-20 10:40:09 +0530

    uploaded

---
+++ OOPS/src/shapes/Circle.java
@@ -0,0 +1,39 @@
+package shapes;
+
+public class Circle  extends Shape{
+	private int radius;
+	
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public void area() {
+		// TODO Auto-generated method stub
+		 double area=3.14*radius*radius;
+		 System.out.println("area of circle =" +area);
+	}
+
+	@Override
+	public void circumference() {
+		// TODO Auto-generated method stub
+		double circum=2*3.14*radius;
+		System.out.println("Circumference of circle = "+circum);
+		
+	}
+
+   @Override
+	public void set() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("entered radius = " +radius);
+	}
+@Override
+  public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("Demo of abstract classes");
+		
+	}
+
+}
