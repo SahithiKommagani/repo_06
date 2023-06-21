fcfce758cc222afd8c61fe08b035065353688ee4
Sahithi Kommagani <sahithi.kommagani@hitachivantara.com>
2023-06-21 16:42:59 +0530

    uploaded

---
+++ OOPS/src/client/Driver.java
@@ -0,0 +1,19 @@
+package client;
+
+import shapes.Circle;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c=new Circle();
+		c.setRadius(5);
+		c.area();
+		c.circumference();
+		c.print();
+		c.set();
+		
+
+	}
+
+}
---
+++ OOPS/src/shapes/Shape.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public abstract class Shape {
+	abstract void area();
+	abstract void circumference();
+	abstract void set();
+	abstract void print();
+	
+
+}
